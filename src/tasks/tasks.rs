bb19eddbbd761eb91e18d73b05e38d04ae36baac
Omar El-Begawy <omar.elbegawy@gmail.com>
2017-09-23 10:56:33 +0300

    added resources

--- src/tasks/tasks.rs
+++ src/tasks/tasks.rs
@@ -1,15 +1,32 @@
+//use std::task;
 use std::thread::spawn;
+
+// not working
+// sources consulted:
+// http://www.rustforrubyists.com/book/chapter-06.html
+// http://aml3.github.io/RustTutorial/html/04.html
+// http://bettong.net/2014/08/01/learning-rust-tasks-and-messages-part-2/
 
 static NTASKS: i32 = 10;
 
 // spawn no work
-//      spawn(proc() {
-//    });
+//do spawn {
+//}
+//  for i in range(0u, NTASKS) {
 
-fn main(){
-  for i in 0..NTASKS {
-    do spawn {
-      println!("this is task nr {}", i)
-    }
+
+//fn main(){
+//  for i in 0..NTASKS {
+//    spawn(proc() {
+//      println!("this is task nr {}", i);
+//    });
+//  }
+//}
+
+fn main() {
+  for i in 0..NTASKS{
+    spawn(|| {
+      println!("this");
+    });
   }
 }
